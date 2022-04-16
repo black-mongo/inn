4d28d40915fc5bc4a0209799f88c611320a2c577
Cam <yangcancai0112@gmail.com>
2022-04-16 21:53:11 +0800

    [main] Try to cargo run

--- inn/src/main.rs
+++ inn/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+//-------------------------------------------------------------------
+// MIT License
+// Copyright (c) 2022 black-mongo
+// @author CameronYang
+// @doc
+//
+// @end
+// Created : 2022-04-14T11:20:42+08:00
+//-------------------------------------------------------------------
+use actix::System;
+use network::NetWork;
+#[actix_rt::main]
+async fn main() {
+    NetWork.start("127.0.0.1", 4556, || {}).await;
+    System::current().stop();
 }
