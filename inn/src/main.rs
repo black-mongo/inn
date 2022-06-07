c58bcea873c8364fca00b6db10ffd8b7fe3d3dc8
Cam <yangcancai0112@gmail.com>
2022-06-07 17:24:55 +0800

    [ws] Add ws listening

--- inn/src/main.rs
+++ inn/src/main.rs
@@ -7,28 +7,25 @@
 // @end
 // Created : 2022-04-14T11:20:42+08:00
 //-------------------------------------------------------------------
-use actix::{Actor, System};
+use actix::Actor;
 use inn_network::server::ProxyServer;
+use inn_network::ws;
 use inn_network::{proxy::Proxy, NetWork};
 #[actix_rt::main]
 async fn main() {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("debug"));
     //
     let server = ProxyServer::default().start();
-    let sock5 = async {
-        let _ = NetWork
-            .start("127.0.0.1", 4556, || {}, server.clone())
-            .await;
-    };
-    let http_proxy = async {
+    let s = server.clone();
+    let _ = NetWork.start("127.0.0.1", 4556, || {}, server.clone());
+    actix::spawn(async move {
         Proxy::start_proxy(
             "127.0.0.1:4557",
             "ca/ca/cacert.pem",
             "ca/ca/cakey.pem",
-            server.clone(),
+            s.clone(),
         )
-        .await;
-    };
-    tokio::join!(sock5, http_proxy);
-    System::current().stop();
+        .await
+    });
+    let _ = ws::run("127.0.0.1".to_string(), 4558, server.clone()).await;
 }
