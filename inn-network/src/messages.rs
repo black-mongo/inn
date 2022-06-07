36d9bfd6f3dc98e113415ff8fee62d6e171bb30d
Cam <yangcancai0112@gmail.com>
2022-06-07 17:23:02 +0800

    [ws] Add websocket msg format

--- inn-network/src/messages.rs
+++ inn-network/src/messages.rs
@@ -8,15 +8,16 @@
 // Created : 2022-04-15T11:16:30+08:00
 //-------------------------------------------------------------------
 
+use std::collections::HashMap;
+
 use actix::Message;
 use actix::Recipient;
-use http::HeaderMap;
-use http::StatusCode;
-use http::Uri;
+use serde::Deserialize;
+use serde::Serialize;
 
 use crate::codec::DstAddress;
 // Command Message to session actor
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum ToSession {
     Ping,
     Stop,
@@ -46,19 +47,15 @@ pub enum ToProxyServer {
     DisConnect(usize),
     OnlineCounter(usize),
     Cli(usize, inn_common::cli::Cli),
-    HttpReq {
-        uri: Uri,
-        headers: HeaderMap,
-        status: StatusCode,
-        error: String,
-    },
+    HttpReq(Box<WsHttpReq>),
 }
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum ProxyServerReply {
     Id(usize),
     OnlineCounter(usize),
     Ok,
     Cli(inn_common::cli::Cli),
+    HttpReq(Box<WsHttpReq>),
 }
 impl Message for ToProxyServer {
     type Result = ProxyServerReply;
@@ -73,4 +70,32 @@ pub enum ForwardReply {
 }
 impl Message for ToFoward {
     type Result = ForwardReply;
+}
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum MsgName {
+    HttpReq,
+}
+#[derive(Serialize, Deserialize, Clone, Default, Debug, PartialEq)]
+#[serde(rename_all = "lowercase")]
+pub struct WsHttpReq {
+    pub id: String,
+    pub uri: String,
+    pub headers: HashMap<String, String>,
+    pub status: u16,
+    pub error: String,
+    pub method: String,
+    pub req_body: String,
+    pub time: String,
+    pub host: String,
+    pub server_ip: String,
+    pub protocol: String,
+    pub resp_headers: HashMap<String, String>,
+    pub resp_body: String,
+}
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub struct WsMsg<T> {
+    pub msg_name: MsgName,
+    pub msg: T,
 }
